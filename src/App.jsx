9674c888bbe27facdd561a309b8e347a54332617
sorazama <gabrielmaciels@hotmail.com>
2024-04-25 09:50:30 -0300

    inserindo a Home

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Escocia from './assets/componentes/escocia'
 import Grand_Canyon from './assets/componentes/grand_canyon'
 import Muralha from './assets/componentes/muralha'
 import Aruba from './assets/componentes/aruba'
+import Home from './assets/componentes/home'
 
 function App() {
 
@@ -11,11 +12,11 @@ function App() {
     
     <div>
       <Header />
+      <Home />
       <Escocia />
       <Grand_Canyon />
       <Muralha />
       <Aruba />
-
     </div>
 
 
